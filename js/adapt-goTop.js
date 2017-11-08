c441f4a91f0996cdb8c6b96f0b4eeba752cb1351
Simon  Date <simondate@yahoo.co.uk>
2017-11-08 11:32:55 +0000

    Component in working state.

--- js/adapt-goTop.js
+++ js/adapt-goTop.js
@@ -4,20 +4,34 @@ define([
 
   var goTopView = Backbone.View.extend({
 
+    tagName: 'button',
+    className: 'goTop',
+
+    events: {
+      'click': 'goTop'
+    },
+
     initialize: function() {
-      console.log('made');
       this.listenTo(Adapt, 'remove', this.remove);
-      var template = Handlebars.templates["goTop"];
-      $('#wrapper').append(template());
+      var template = Handlebars.templates.goTop;
+      $('#wrapper').append(this.$el.html(template()));
+    },
+
+    goTop: function() {
+      console.log(Adapt.location);
+      var $page = $('.' + Adapt.location._currentId);
+      console.log($page);
+      Adapt.scrollTo($page, {
+        duration: 400
+      });
     }
   });
 
   Adapt.on("pageView:ready", function() {
     var model = Adapt.course.get('_goTop');
-    console.log('ere');
-    if(!model || !model._isEnabled) return;
+    if (!model || !model._isEnabled) return;
     new goTopView();
   });
 
-    return goTopView;
+  return goTopView;
 });
